2f242bb719a6d72323e5a7bb7944a0caec8317e7
alecswladpachomow <alecswlad@yandex.ru>
2022-03-06 22:52:04 +0300

    modified:   Main.java

--- Main.java
+++ Main.java
@@ -2,16 +2,27 @@ package numbers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Main {
 
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
 
-    List<Integer> outList = new ArrayList();
-        for (int i = 0; i <intList.size()-1 ; i++) {
-
+        List<Integer> outList = new ArrayList();
+        for (int i = 0; i < intList.size(); i++) {
+            if (intList.get(i) > 0 &&
+                    intList.get(i) % 2 == 0) outList.add(intList.get(i));
         }
+        outList.sort(Comparator.naturalOrder());
+        outList.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer);
+            }
+        });
+
     }
 }
